f3d7b3240369f40068e4c301f0e4d6b501f3e915
Git-Seema <seematshepo19@gmail.com>
2024-10-16 00:37:59 +0200

    feat: add core elevator interfaces for flexible elevator behavior added IElevator, IElevatorDispatchStrategy and IFloor Interface to manage floor-level operations

---
+++ Interfaces/IFloor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulationApp.Interface_Classes
+{
+    internal class IFloor
+    {
+    }
+}
